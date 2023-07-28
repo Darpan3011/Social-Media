ee1b026b070121c6021562d0bd2219cdd8e37ea6
Darpan3011 <darpankanani301@gmail.com>
2023-07-29 00:02:38 +0530

    change link

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -19,7 +19,7 @@ const Login = () => {
     if(email === '' || password === '') return
 
     try {
-      const res = await fetch(`http://localhost:5000/auth/login`, {
+      const res = await fetch(`https://backend-udbf.onrender.com/auth/login`, {
         headers: {
           'Content-Type': 'application/json',
         },
